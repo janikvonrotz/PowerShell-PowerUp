abac348b56e66b86e9c7d5b921c4755a30d27e19
Janik von Rotz <contact@janikvonrotz.ch>
2013-03-04 08:45:33 +0100

    Added PsGet to the default modules

--- Modules/PoSHServer/http/example4.ps1
+++
@@ -1,7 +0,0 @@
-# You can also use html files as templates
-# Let's import one of them
-# But we only get content of html files, you can't execute Powershell in them.
-@"
-$(Write-Host Hello World Example4!)
-$(Get-Content "$HomeDirectory\index.htm")
-"@
