529d923a37a5451da96940b8497a9beaede708b7
Janik von Rotz <contact@janikvonrotz.ch>
2013-03-12 14:58:39 +0100

    Update
    Yeay

--- Functions/Get-AvailableDriveLetter.ps1
+++ Functions/Get-AvailableDriveLetter.ps1
@@ -1,41 +1,49 @@
-﻿#--------------------------------------------------#
-#  Title: Get Available Drive Letter
-#--------------------------------------------------#
-#  File name: Get-AvailableDriveLetter.ps1
-#  Description: [Description]
-#  Tags: powershell, functions
-#  Project: [Projectname]
-#
-#  Author: Janik von Rotz
-#  Author E-Mail: contact@janikvonrotz.ch
-#
-#  Create Date: 11.02.2013
-#  Last Edit Date: 11.02.2013
-#  Version: 1.0.0
-#
-#  License:
-#  This work is licensed under the Creative Commons Attribution-NonCommercial-NoDerivs 3.0 Unported License.
-#  To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-nd/3.0/ or
-#  send a letter to Creative Commons, 444 Castro Street, Suite 900, Mountain View, California, 94041, USA.
-#--------------------------------------------------#
+﻿$Metadata = @{
+	Title = "Get Available Drive Letter"
+	Filename = "Get-AvailableDriveLetter.ps1"
+	Description = ""
+	Tags = "powershell, functions"
+	Project = ""
+	Author = "Janik von Rotz"
+	AuthorEMail = "contact@janikvonrotz.ch"
+	CreateDate = "11.02.2013"
+	LastEditDate = "12.03.2013"
+	Version = "1.0.2"
+	License = @'
+This work is licensed under the Creative Commons Attribution-NonCommercial-NoDerivs 3.0 Unported License.
+To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-nd/3.0/ or
+send a letter to Creative Commons, 444 Castro Street, Suite 900, Mountain View, California, 94041, USA.
+'@
+}
 
 Function Get-AvailableDriveLetter {
 
 	param(
 		[parameter(Mandatory=$False)]
 		[Switch]
-		$ReturnFirstLetterOnly
+		$ReturnFirstLetterOnly,
+		[parameter(Mandatory=$False)]
+		$FavoriteDriveLetter
 	)
-	 
-    $TempDriveLetters = [char[]]'CDEFGHIJKLMNOPQRSTUVWXYZ' | ? { (Get-PSDrive $_ -ErrorAction 'SilentlyContinue') -eq $null }
+    
+	if($ReturnFirstLetterOnly -eq $true -and $FavoriteDriveLetter -ne $null){
+		throw "Only one parameter is possible for this function"
+		exit
+	}	
+	
+	# Removed letters A, B, C and D for possible free drive letters	
+    $TempDriveLetters = [char[]]'EFGHIJKLMNOPQRSTUVWXYZ' | ? { (Get-PSDrive $_ -ErrorAction 'SilentlyContinue') -eq $null }
+
 
 	if ($ReturnFirstLetterOnly -eq $true)
 	{
 		$TempDriveLetters[0]
 	}
-	else
-	{
-		$TempDriveLetters
-	}
-	
+	elseif($TempDriveLetters.contains($FavoriteDriveLetter)){
+	    $FavoriteDriveLetter
+    }elseif($FavoriteDriveLetter -ne $null){
+		$TempDriveLetters[0]
+    }else{
+	    $TempDriveLetters
+    }		
 }
--- Functions/Get-JsonConfig.ps1
+++ Functions/Get-JsonConfig.ps1
@@ -1,31 +1,36 @@
-#--------------------------------------------------#
-#  Title: Get Json Configuration
-#--------------------------------------------------#
-#  File name: Get-JsonConfig.ps1
-#  Description: [Description]
-#  Tags: powershell, functions
-#  Project: [Projectname]
-#
-#  Author: Janik von Rotz
-#  Author E-Mail: contact@janikvonrotz.ch
-#
-#  Create Date: 18.02.2013
-#  Last Edit Date: 18.02.2013
-#  Version: 1.0.0
-#
-#  License:
-#  This work is licensed under the Creative Commons Attribution-NonCommercial-NoDerivs 3.0 Unported License.
-#  To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-nd/3.0/ or
-#  send a letter to Creative Commons, 444 Castro Street, Suite 900, Mountain View, California, 94041, USA.
-#--------------------------------------------------#
+$Metadata = @{
+	Title = "Get Json Configuration"
+	Filename = "Get-JsonConfig.ps1"
+	Description = ""
+	Tags = "powershell, functions"
+	Project = ""
+	Author = "Janik von Rotz"
+	AuthorEMail = "contact@janikvonrotz.ch"
+	CreateDate = "18.02.2013"
+	LastEditDate = "12.03.2013"
+	Version = "1.0.1"
+	License = @'
+This work is licensed under the Creative Commons Attribution-NonCommercial-NoDerivs 3.0 Unported License.
+To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-nd/3.0/ or
+send a letter to Creative Commons, 444 Castro Street, Suite 900, Mountain View, California, 94041, USA.
+'@
+}
 
 function Get-JsonConfig {
 	param (
         [parameter(Mandatory=$true)]
 		$Path
 	)
+	<#
+	#--------------------------------------------------#
+	# Example
+	#--------------------------------------------------#
+		
+	$Config = Get-JsonConfig -Path ".\Create-PSSession.config.json.ps1" 
 	
-	 #Import
-    Get-ChildItem | Where-Object {$_.Name -match $Path} | foreach {. .\$_}
+	#>
+	
+	#Import
+    $Content = &($Path)
 	return $Configuration = ($Content | ConvertFrom-Json)
 }
