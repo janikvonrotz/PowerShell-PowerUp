080cc8306dc997c84673384216a7e17b2001d9ad
Janik von Rotz <contact@janikvonrotz.ch>
2013-03-27 14:21:25 +0100

    fix

--- PublicScripts/Create-Reports.ps1
+++ PublicScripts/Create-Reports.ps1
@@ -6,8 +6,12 @@ if($NoExit -eq $false){
 $s1 = Create-PSSession sharepoint
 $s2 = Create-PSSession data03
 
-$Report = Invoke-Command -Session $s1 -ScriptBlock {D:\Powershell-Profile\PublicScripts\SPSecurableObjectPermissions\Report-SPSecurableObjectPermissions.ps1}
+$ReportUserGroups = Invoke-Command -Session $s1 -ScriptBlock {(D:\Powershell-Profile\PublicScripts\ADUserGroups\Report-ActiveDirectoryUserGroups.ps1 -Usernames "vonrotz")}
+$Report1 = Invoke-Command -Session $s1 -ScriptBlock {D:\Powershell-Profile\PublicScripts\SPSecurableObjectPermissions\Report-SPSecurableObjectPermissions.ps1}
 $Report2 = Invoke-Command -Session $s2 -ScriptBlock {(C:\Powershell-Profile\PublicScripts\FileSystemPermissions\Report-FileSystemPermissions#NoDependencies.ps1 -Path "F:\Dat" -Levels 3)}
 
-$Reports = $Report + $Report2
-$Reports | Out-GridView
+$Report = $Report1 + $Report2
+
+$Report | Out-GridView
+
+$ReportUserGroups | Out-GridView
