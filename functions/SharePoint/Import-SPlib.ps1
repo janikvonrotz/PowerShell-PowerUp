386b9c4acd8e3b96266a8ace5a2c8ef8d3920729
Janik von Rotz <contact@janikvonrotz.ch>
2013-04-26 16:04:29 +0200

    wrong function :(

--- functions/SharePoint/Import-SPlib.ps1
+++ functions/SharePoint/Import-SPlib.ps1
@@ -63,6 +63,14 @@ send a letter to Creative Commons, 444 Castro Street, Suite 900, Mountain View, 
 	$SiteUrl = Get-SPUrl $SiteUrl -SiteUrl
 	
 	Write-Warning ("Importing " + $Path + " to " + $HostSiteUrl)
-	Import-SPWeb $HostSiteUrl -path $Path -IncludeUserSecurity -nologfile -Force
+	<#
+	Error Workaround: Second import
+	Import-SPWeb : <nativehr>0x80070003</nativehr><nativestack></nativestack>
+	#>
+	try{
+		Import-SPWeb $HostSiteUrl -path $Path -IncludeUserSecurity -nologfile -Force
+	}catch{
+		Import-SPWeb $HostSiteUrl -path $Path -IncludeUserSecurity -nologfile -Force	
+	}
 	Write-Host "Finished" -BackgroundColor Black -ForegroundColor Green
 }
