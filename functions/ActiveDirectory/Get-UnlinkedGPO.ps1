49e9afa1a664a1977059a233e197a33a463cca16
Janik von Rotz <contact@janikvonrotz.ch>
2013-05-06 17:08:33 +0200

    New function Get-UnlinkedGPO

---
+++ functions/ActiveDirectory/Get-UnlinkedGPO.ps1
@@ -0,0 +1,58 @@
+function Get-UnlinkedGPO {
+<#
+.SYNOPSIS
+	Get all unlinkend GPOs
+ 
+.EXAMPLE
+	PS C:\> Get-UnlinkedGPO
+#>
+
+$Metadata = @{
+	Title = "Get unlinked GPOs"
+	Filename = "Get-UnlinkedGPO.ps1"
+	Description = ""
+	Tags = "powershell, function, activedirectory"
+	Project = ""
+	Author = "Janik von Rotz"
+	AuthorContact = "www.janikvonrotz.ch"
+	CreateDate = "2013-05-06"
+	LastEditDate = "2013-05-06"
+	Version = "1.0.0"
+	License = @'
+This work is licensed under the Creative Commons Attribution-NonCommercial-NoDerivs 3.0 Unported License.
+To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-nd/3.0/ or
+send a letter to Creative Commons, 444 Castro Street, Suite 900, Mountain View, California, 94041, USA.
+'@
+}
+
+	#--------------------------------------------------#
+	# modules
+	#--------------------------------------------------#
+    Import-module grouppolicy 
+    
+	#--------------------------------------------------#
+	# helper and functions
+	#--------------------------------------------------#
+    function IsNotLinked($xmldata){ 
+        If ($xmldata.GPO.LinksTo -eq $null) { 
+            Return $true 
+        } 
+         
+        Return $false 
+    } 
+    
+	#--------------------------------------------------#
+	# main
+	#--------------------------------------------------#
+    
+    $Report = @() 
+     
+    Get-GPO -All | ForEach { $GPO = $_ ; $_ | Get-GPOReport -ReportType xml | ForEach { If(IsNotLinked([xml]$_)){$Report += $GPO} }} 
+     
+    If ($unlinkedGPOs.Count -eq 0) { 
+        Wirte-Warning "No unlinked GPO's found" 
+    }else{ 
+        return $Report
+    }
+}
+Get-UnlinkedGPO
