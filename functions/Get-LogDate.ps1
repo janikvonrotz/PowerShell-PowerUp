a0f2a1502a2c207a63e990654affe090f1d184e5
Janik von Rotz <contact@janikvonrotz.ch>
2013-05-08 16:07:46 +0200

    convert logstamp to datetime

--- functions/Get-LogDate.ps1
+++ functions/Get-LogDate.ps1
@@ -1,34 +1,43 @@
-function Get-LogStamp {
+function Get-LogDate {
 <#
 .SYNOPSIS
-	datetime to logstamp string 
+	logstamp string to datetime
 
 .DESCRIPTION
-	Returns a logstamp string from now datetime
+	Returns the datetime from a logstamp string
+
+.PARAMETER  LogStamp
+	A logstamp string from the command Get-LogStamp
 
 .EXAMPLE
-	PS C:\> Get-LogStamp
+	PS C:\> Get-LogDate -LogStamp $String
 
 .OUTPUTS
-	logstamp string
+	System.datetime
 #>
 
+param(
+		[Parameter(Mandatory=$true)]
+		[String]
+		$LogStamp
+)
+
 	$Metadata = @{
-		Title = "Get Logstamp"
-		Filename = "Get-LogStamp.ps1"
+		Title = "Get Log Date"
+		Filename = "Get-LogDate.ps1"
 		Description = ""
 		Tags = "powershell, functions"
 		Project = ""
 		Author = "Janik von Rotz"
 		AuthorContact = "www.janikvonrotz.ch"
-		CreateDate = "2013-01-02"
-		LastEditDate = "2013-03-25"
-		Version = "1.0.1"
+		CreateDate = "2013-05-08"
+		LastEditDate = "2013-05-08"
+		Version = "1.0.0"
 		License = @'
 This work is licensed under the Creative Commons Attribution-NonCommercial-NoDerivs 3.0 Unported License.
 To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-nd/3.0/ or
 send a letter to Creative Commons, 444 Castro Street, Suite 900, Mountain View, California, 94041, USA.
 '@
 }
-	return $(Get-Date -Format "yyyy-MM-dd HH-mm-ss")
+	return [datetime]::ParseExact($LogStamp, "yyyy-MM-dd HH-mm-ss", $null)
 }
