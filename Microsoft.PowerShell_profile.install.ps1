186e210e28b320a3a6bade7f09c9a0dcfc0caa9b
Janik von Rotz <contact@janikvonrotz.ch>
2013-05-14 12:37:24 +0200

    name conflict

--- Microsoft.PowerShell_profile.install.ps1
+++ Microsoft.PowerShell_profile.install.ps1
@@ -146,7 +146,7 @@ $Metadata = @{
 	Author = "Janik von Rotz"
 	AuthorContact = "www.janikvonrotz.ch"
 	CreateDate = "2013-04-22"
-	LastEditDate = "2013-04-22"
+	LastEditDate = "2013-05-14"
 	Version = "3.1.0"
 	License = "This work is licensed under the Creative Commons Attribution-NonCommercial-NoDerivs 3.0 Unported License. To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-nd/3.0/ or send a letter to Creative Commons, 444 Castro Street, Suite 900, Mountain View, California, 94041, USA."
 }
@@ -165,7 +165,7 @@ $Metadata = @{
 	Author = "Janik von Rotz"
 	AuthorContact = "www.janikvonrotz.ch"
 	CreateDate = "2013-04-22"
-	LastEditDate = "2013-04-22"
+	LastEditDate = "2013-05-14"
 	Version = "3.1.0"
 	License = "This work is licensed under the Creative Commons Attribution-NonCommercial-NoDerivs 3.0 Unported License. To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-nd/3.0/ or send a letter to Creative Commons, 444 Castro Street, Suite 900, Mountain View, California, 94041, USA."
 }
@@ -240,8 +240,8 @@ foreach ($IncludeFolder in $IncludeFolders){
 nal -Name rdp -Value "Connect-RDPSession"
 nal -Name rps -Value "Connect-PSSession"
 nal -Name http -Value "Connect-HttpSession"
-nal -Name ssh -Value "Connect-SSHSession"
-nal -Name scp -Value "Connect-SCPSession"
+nal -Name psssh -Value "Connect-SSHSession"
+nal -Name psscp -Value "Connect-SCPSession"
 
 '@
 	Write-Warning "`nAdded Custom Aliases to the profile script"
