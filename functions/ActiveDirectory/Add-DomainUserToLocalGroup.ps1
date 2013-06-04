828b71538a017455cb7c29a1c3ca673de644142a
Janik von Rotz <contact@janikvonrotz.ch>
2013-06-04 16:24:44 +0200

    New function

---
+++ functions/ActiveDirectory/Add-DomainUserToLocalGroup.ps1
@@ -0,0 +1,72 @@
+function Add-DomainUserToLocalGroup{
+
+<#
+.DESCRIPTION
+	Add a domain user to a computer local group
+
+.PARAMETER  Domain
+	The domain which the user belongs to
+	
+.PARAMETER  User
+	Name of the domain user
+
+.PARAMETER  Group
+	Local GroupName
+	
+.PARAMETER  Computer
+	Computer name to process this command
+	
+.EXAMPLE
+	Add-DomainUserToLocalGroup -Domain contoso.com -User User1 -Group Administrators -Computer Server1.contoso.com
+#>
+	
+	param(
+		[Parameter(Mandatory=$true)]
+		[String]
+		$Domain,
+		
+		[Parameter(Mandatory=$true)]
+		[String[]]
+		$User,
+		
+		[Parameter(Mandatory=$true)]
+		[String[]]
+		$Group,
+		
+		[Parameter(Mandatory=$true)]
+		[String[]]
+		$Computer
+	)
+
+	$Metadata = @{
+		Title = "Add Domain User To Local Group"
+		Filename = "Add-DomainUserToLocalGroup.ps1"
+		Description = ""
+		Tags = "powershell, function, activedirectory"
+		Project = ""
+		Author = "Janik von Rotz"
+		AuthorContact = "www.janikvonrotz.ch"
+		CreateDate = "2013-06-04"
+		LastEditDate = "2013-06-04"
+		Version = "1.0.0"
+		License = @'
+This work is licensed under the Creative Commons Attribution-NonCommercial-NoDerivs 3.0 Unported License.
+To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-nd/3.0/ or
+send a letter to Creative Commons, 444 Castro Street, Suite 900, Mountain View, California, 94041, USA.
+'@
+}
+
+	#--------------------------------------------------#
+	# Main
+	#--------------------------------------------------#
+	
+	$LocalGroup = [ADSI]"WinNT://$Computer/$Group,group"
+	
+	$DomainUser = [ADSI]"WinNT://$Domain/$User,user"
+	
+	Write-Host "Adding domain user: $User from: $Domain to local group: $Group on computer: $Computer"
+	
+	$LocalGroup.Add($DomainUser.Path)
+}
+
+Add-DomainUserToLocalGroup -Domain vbl.ch -User sp3-setup -Group Administrators -Computer vblw2k12extvr1
