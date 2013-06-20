f694339d8a99bfed4ad7bcdc167ca95abc6d2582
Janik von Rotz <contact@janikvonrotz.ch>
2013-06-20 10:03:40 +0200

    Update connect-ftpsession function

--- functions/RemoteConnection/Connect-FTPSession.ps1
+++ functions/RemoteConnection/Connect-FTPSession.ps1
@@ -1,84 +1,144 @@
-﻿function Connect-FTPSession{
-<#
-	.SYNOPSIS
-		Remote management for ftp sessions
-
-	.DESCRIPTION
-		Starts a ftp session with the parameters from the remote config file.
-
-	.PARAMETER Name
-		Servername (Key or Name from the remote config file for preconfiguration)
-		
-	.PARAMETER User
-		Username (overwrites remote config file parameter)
-		
-	.PARAMETER Port
-		Port (overwrites remote config file parameter)
-
-	.EXAMPLE
-	   Connect-FTPSession -Names firewall
-
-#>
-
-	#--------------------------------------------------#
-	# Parameter
-	#--------------------------------------------------#
-	param (
-        [parameter(Mandatory=$true)][string]
-		$Name,
-        [parameter(Mandatory=$false)][string]
-		$User,
-        [parameter(Mandatory=$false)][int]
-		$Port
-	)
-
-	$Metadata = @{
-		Title = "Connect FTP Session"
-		Filename = "Connect-FTPSession"
-		Description = ""
-		Tags = "powershell, remote, session, ftp"
-		Project = ""
-		Author = "Janik von Rotz"
-		AuthorContact = "www.janikvonrotz.ch"
-		CreateDate = "2013-05-17"
-		LastEditDate = "2013-06-13"
-		Version = "1.0.0"
-		License = @'
+﻿<#
+$Metadata = @{
+	Title = "Connect FTP Session"
+	Filename = "Connect-FTPSession"
+	Description = ""
+	Tags = "powershell, remote, session, ftp, sftp"
+	Project = ""
+	Author = "Janik von Rotz"
+	AuthorContact = "www.janikvonrotz.ch"
+	CreateDate = "2013-05-17"
+	LastEditDate = "2013-06-20"
+	Version = "1.0.0"
+	License = @'
 This work is licensed under the Creative Commons Attribution-NonCommercial-NoDerivs 3.0 Unported License. 
 To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-nd/3.0/ or
 send a letter to Creative Commons, 444 Castro Street, Suite 900, Mountain View, California, 94041, USA.
 '@
 }
+#>
+
+function Connect-FTPSession{
+
+<#
+.SYNOPSIS
+	Remote management for ftp sessions
+
+.DESCRIPTION
+	Starts a ftp session with the parameters from the remote config file.
+
+.PARAMETER Name
+	Servername (Key or Name from the remote config file for preconfiguration)
+	
+.PARAMETER User
+	Username (overwrites remote config file parameter)
+	
+.PARAMETER Port
+	Port (overwrites remote config file parameter)
+
+.PARAMETER  Secure
+	Use sftp instead of ftp
+	
+.PARAMETER  PrivatKey
+	PrivatKey (overwrites remote config file parameter)
+
+.EXAMPLE
+   Connect-FTPSession -Names firewall
+#>
+
+	param (
+        [parameter(Mandatory=$true)][string]
+		$Name,
+		
+        [parameter(Mandatory=$false)][string]
+		$User,
+		
+        [parameter(Mandatory=$false)][int]
+		$Port,
+		
+        [switch]
+		$Secure,
+				
+        [parameter(Mandatory=$false)][string]
+		$PrivatKey
+	)
 
 
 	#--------------------------------------------------#
 	# main
 	#--------------------------------------------------#
 	if (Get-Command "winscp.exe"){ 
-
-		# Load Configurations
+    
+    	# Load Configurations
 		$Server = Get-RemoteConnections -Names $Name -FirstEntry
-
-		# set port
-		# get port from Protocol
-		if(!$Port){
-			$Server.Protocol | %{
-				if ($_.Name -eq "scp" -and $_.Port -ne ""){
-					$Port = $_.Port
-				}
-			}
-		}
-		if(!$Port -or $Port -eq 0){
-			$Port = 21
-		}
+        
+        # default settings
+        $SftpPort = 22
+        $FtpPort = 21		
+		
+        # set port and protocol from config file
+        if(!$Port){
+            
+            # cycle thour protoc configs
+    		$Server.Protocol | %{
+            
+                # settings for ftp
+    			if($_.Name -eq "ftp"){
+                                        
+    				$Protocol = "ftp"
+                    
+    				if($_.Port -ne ""){
+                    
+    					$Port = $_.Port
+    				}else{
+                        $Port = $FtpPort
+                    }
+                    
+                # settings for sftp
+    			}elseif($_.Name -eq "sftp"){
+                
+					$Protocol = "sftp"
+                    
+					if($_.Port -ne ""){
+						$Port = $_.Port
+                    }else{
+                        $Port = $SftpPort
+                    }              
+               }
+           }        
+        }
+        
+        # if there is no configuration from config file
+        if(!$Port -or $Port -eq 0){
+            if($Secure){
+                $Port = $SftpPort
+            }else{
+                $Port = $FtpPort
+            }
+        }
+       
+        # settings for secure parameter
+        if(!$Protocol){
+            if($Secure){
+                $Protocol = "sftp"
+            }else{
+                $Protocol = "ftp"
+            }
+        }
 
 		# set servername
 		$Servername = $Server.Name
-
+				
 		# set username
 		if(!$User){$User = $Server.User}
-
-        Invoke-Expression ("WinSCP.exe ftp://$User@$Servername" + ":$Port")
+		
+        # set privatkey
+        if(!$PrivatKey){$PrivatKey = Invoke-Expression ($Command = '"' + $Server.PrivatKey + '"')}
         
+        if($PrivatKey){
+            Invoke-Expression ("WinSCP.exe $Protocol"+"://$User@$Servername"+":$Port"+" /privatekey='$PrivatKey'")
+        }else{
+            Invoke-Expression ("WinSCP.exe $Protocol"+"://$User@$Servername"+":$Port")
+        }
     }
 }
