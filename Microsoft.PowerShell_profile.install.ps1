d90919f1326e9d56cb24450d8bfe92a359da0771
Janik von Rotz <contact@janikvonrotz.ch>
2013-10-02 13:58:51 +0200

    renaming remote management
    attionzione

--- Microsoft.PowerShell_profile.install.ps1
+++ Microsoft.PowerShell_profile.install.ps1
@@ -255,13 +255,16 @@ if($Features | Where{$_.Name -eq "Custom Aliases"}){
 
 #--------------------------------------------------#
 # Custom Aliases
-#--------------------------------------------------#	
-nal -Name crdp -Value "Connect-RDPSession" -ErrorAction SilentlyContinue
-nal -Name crps -Value "Connect-PSSession" -ErrorAction SilentlyContinue
-nal -Name chttp -Value "Connect-HttpSession" -ErrorAction SilentlyContinue
-nal -Name cssh -Value "Connect-SSHSession" -ErrorAction SilentlyContinue
-nal -Name cscp -Value "Connect-SCPSession" -ErrorAction SilentlyContinue
-nal -Name cftp -Value "Connect-FTPSession" -ErrorAction SilentlyContinue
+#--------------------------------------------------#
+nal -Name grc -Value "Get-RemoteConnection" -ErrorAction SilentlyContinue	
+nal -Name crdp -Value "Connect-RDP" -ErrorAction SilentlyContinue
+nal -Name crps -Value "Connect-PSS" -ErrorAction SilentlyContinue
+nal -Name chttp -Value "Connect-Http" -ErrorAction SilentlyContinue
+nal -Name cssh -Value "Connect-SSH" -ErrorAction SilentlyContinue
+nal -Name cscp -Value "Connect-SCP" -ErrorAction SilentlyContinue
+nal -Name cftp -Value "Connect-FTP" -ErrorAction SilentlyContinue
+nal -Name cvss -Value "Connect-VSphere" -ErrorAction SilentlyContinue
+nal -Name cvm -Value "Connect-VM" -ErrorAction SilentlyContinue
 
 '@
     $PSPContentISE += $Content
