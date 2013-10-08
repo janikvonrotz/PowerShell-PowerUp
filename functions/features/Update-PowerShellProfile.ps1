5835f7f0574c8b8278c7ad3a6ccaad86071fe33d
Janik von Rotz <contact@janikvonrotz.ch>
2013-10-08 14:02:56 +0200

    fix updater

--- functions/features/Update-PowerShellProfile.ps1
+++ functions/features/Update-PowerShellProfile.ps1
@@ -5,23 +5,22 @@ function Update-PowerShellProfile{
 		cinst git -force
 	}
 
+	cd $PSProfilePath
+	
 	if(!(Test-Path -Path (Join-Path -Path $PSProfile.Path -ChildPath ".git"))){
-		
-		cd $PSProfilePath
 
 		# initialise git repository
 		git init
 		git remote add origin "git://github.com/janikvonrotz/Powershell-Profile.git"
 		git fetch origin
 		git reset --hard origin/master
-
-
-		Pop-Location -StackName "WorkingPath"
-
+		
 	}else{
 
 		git fetch origin
 		git reset --hard origin/master
 
 	}
+	
+	Pop-Location -StackName "WorkingPath"
 }
