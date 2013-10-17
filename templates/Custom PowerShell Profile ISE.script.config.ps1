6993f1cefa00e568f78dd142c0c071b6cdbdb1b4
Janik von Rotz <login@janikvonrotz.ch>
2013-10-17 10:05:48 +0200

    custom scripts

---
+++ templates/Custom PowerShell Profile ISE.script.config.ps1
@@ -0,0 +1,11 @@
+# Custom Aliases
+nal -Name grc -Value "Get-RemoteConnection" -ErrorAction SilentlyContinue
+nal -Name gppc -Value "Get-PPConfiguration" -ErrorAction SilentlyContinue	
+nal -Name crdp -Value "Connect-RDP" -ErrorAction SilentlyContinue
+nal -Name crps -Value "Connect-PSS" -ErrorAction SilentlyContinue
+nal -Name chttp -Value "Connect-Http" -ErrorAction SilentlyContinue
+nal -Name cssh -Value "Connect-SSH" -ErrorAction SilentlyContinue
+nal -Name cscp -Value "Connect-SCP" -ErrorAction SilentlyContinue
+nal -Name cftp -Value "Connect-FTP" -ErrorAction SilentlyContinue
+nal -Name cvss -Value "Connect-VSphere" -ErrorAction SilentlyContinue
+nal -Name cvm -Value "Connect-VM" -ErrorAction SilentlyContinue
