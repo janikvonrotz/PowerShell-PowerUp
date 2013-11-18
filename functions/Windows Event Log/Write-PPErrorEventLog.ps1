222df3c3b534b3e2bacb656a47226492b2a4e896
Janik von Rotz <login@janikvonrotz.ch>
2013-11-18 17:04:30 +0100

    asödlfkasd

--- functions/Windows Event Log/Write-PPErrorEventLog.ps1
+++ functions/Windows Event Log/Write-PPErrorEventLog.ps1
@@ -8,7 +8,7 @@ $Metadata = @{
 	Author = "Janik von Rotz"
 	AuthorContact = "http://janikvonrotz.ch"
 	CreateDate = "2013-10-21"
-	LastEditDate = "2013-10-25"
+	LastEditDate = "2013-11-18"
 	Version = "1.0.0"
 	License = @'
 This work is licensed under the Creative Commons Attribution-ShareAlike 3.0 Switzerland License.
@@ -27,8 +27,8 @@ function Write-PPErrorEventLog{
 .DESCRIPTION
 	If an error occured this function writes an event log.
 
-.PARAMETER  ScriptPath
-	Error event source.
+.PARAMETER  Message
+	Message for the event log entry. The Error log will be add after this message.
 
 .PARAMETER  Source
 	Source parameter for the event log entry.
@@ -37,15 +37,15 @@ function Write-PPErrorEventLog{
 	Clear PowerShell error variable.
 	
 .EXAMPLE
-	PS C:\> Write-PPErrorEventLog -Source $MyInvocation.InvocationName
+	PS C:\> Write-PPErrorEventLog -Message "An error occured!" -Source "MyApplication" -ClearErrorVariable
 #>
 
 	param(        
-        [Parameter(Mandatory=$true)]
-		$ScriptPath,
+        [Parameter(Mandatory=$false)]
+		[string]$Message,
         
         [Parameter(Mandatory=$false)]
-		$Source,
+		[string]$Source,
 		
 		[switch]
 		$ClearErrorVariable  
@@ -54,14 +54,13 @@ function Write-PPErrorEventLog{
 	#--------------------------------------------------#
 	# main
 	#--------------------------------------------------#   
-    if($Error){    
-        $Message = $ScriptPath
-        $Error | %{$Message += "$($_.ToString()) $($_.InvocationInfo.PositionMessage) `n`n"}
+    if($Error){
+        $Error | %{$ErrorLog += "$($_.ToString()) $($_.InvocationInfo.PositionMessage) `n`n"}
         
-        $Message = "$ScriptPath `n`n" + $Message
+        if($Message){$ErrorLog = "$Message `n`n" + $ErrorLog}
         
         if($ClearErrorVariable){$Error.clear()}
         
-        Write-PPEventLog -Message $Message -Source $Source -EntryType Error -AppendSessionLog
+        Write-PPEventLog -Message $ErrorLog -Source $Source -EntryType Error -AppendSessionLog
     }
 }
