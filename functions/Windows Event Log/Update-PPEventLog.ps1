100a51aa6382e9df6fdf388f0e6153a1c4cc143c
Janik von Rotz <login@janikvonrotz.ch>
2014-01-29 16:05:25 +0100

    update function
    alternative does not work

--- functions/Windows Event Log/Update-PPEventLog.ps1
+++ functions/Windows Event Log/Update-PPEventLog.ps1
@@ -46,23 +46,23 @@ function Update-PPEventLog{
 			
 			Write-Host "Create event log: $($_.Name)"
 			New-EventLog -LogName $_.Name -Source $_.Source -ErrorAction SilentlyContinue
+			$EventLog = Get-WmiObject win32_nteventlogfile -filter "filename='$($_.Name)'"
 			
-		}else{
+		
         
-            $SourcesTO = ($_.Source | %{"$($_.Name)"}) + ($_.Source | where{$_.Expression} | %{$_.Expression | iex})
-            $SourcesIS = $EventLog.Sources
-			
-            # add sources
-            Compare-Object $SourcesTO $SourcesIS -passThru | Where-Object{ $_.SideIndicator -eq '<=' } | %{
-				Write-Host "Add source: $_ to event log: $($EventLog.LogfileName)"
-                New-EventLog -Source $_ -LogName $EventLog.LogfileName
-            }
-            
-            # remove sources
-            Compare-Object $SourcesTO $SourcesIS -passThru | Where-Object{ $_.SideIndicator -eq '=>' } | %{
-				Write-Host "Remove source: $_ from event log: $($EventLog.LogfileName)"
-                Remove-EventLog -Source $_
-            }
+		$SourcesTO = ($_.Source | %{"$($_.Name)"}) + ($_.Source | where{$_.Expression} | %{$_.Expression | iex})
+		$SourcesIS = $EventLog.Sources
+		
+		# add sources
+		Compare-Object $SourcesTO $SourcesIS -passThru | Where-Object{ $_.SideIndicator -eq '<=' } | %{
+			Write-Host "Add source: $_ to event log: $($EventLog.LogfileName)"
+			New-EventLog -Source $_ -LogName $EventLog.LogfileName
+		}
+		
+		# remove sources
+		Compare-Object $SourcesTO $SourcesIS -passThru | Where-Object{ $_.SideIndicator -eq '=>' } | %{
+			Write-Host "Remove source: $_ from event log: $($EventLog.LogfileName)"
+			Remove-EventLog -Source $_
 		}
 	}
 }
