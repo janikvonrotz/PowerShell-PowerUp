29f2aa57334457f93b7fab9575c24f4758ae5adc
Janik von Rotz <login@janikvonrotz.ch>
2014-03-14 08:21:53 +0100

    new packages
    aws tools and vmware power cli

---
+++ lib/App/Install-VMware vSphere PowerCLI.ps1
@@ -0,0 +1,97 @@
+param(
+	[string]$Version,
+	[string]$Path,
+	[switch]$Force,
+	$Update,
+	[switch]$Uninstall
+)
+
+#--------------------------------------------------#
+# settings
+#--------------------------------------------------#
+
+$Configs = @{
+	Url = "https://my.vmware.com/group/vmware/details?downloadGroup=PCLI550&productId=352"
+    Path = "$(Split-Path -Path $MyInvocation.MyCommand.Definition -Parent)\"
+}
+
+$Configs | ForEach-Object{
+
+    try{
+
+        $_.Result = $null
+        if(-not $_.Path){$_.Path = $Path}
+        $Config = $_
+
+        #--------------------------------------------------#
+        # add app
+        #--------------------------------------------------#
+
+        if(-not $Uninstall){
+
+            #--------------------------------------------------#
+            # check condition
+            #--------------------------------------------------#
+
+            if($_.ConditionExclusion){            
+                $_.ConditionExclusionResult = $(Invoke-Expression $Config.ConditionExclusion -ErrorAction SilentlyContinue)        
+            }    
+            if(($_.ConditionExclusionResult -eq $null) -or $Force){
+                    	
+                #--------------------------------------------------#
+                # download
+                #--------------------------------------------------#
+                
+                Write-Warning "Installer is not available for public download. Download it on your own from the official VMware website."
+                Start-Process -FilePath "$($_.Url)" 			
+
+                #--------------------------------------------------#
+                # installation
+                #--------------------------------------------------#
+				                		
+                #--------------------------------------------------#
+                # configuration
+                #--------------------------------------------------#	
+                
+                #--------------------------------------------------#
+                # cleanup
+                #--------------------------------------------------#
+                		
+                #--------------------------------------------------#
+                # finisher
+                #--------------------------------------------------#
+                		
+                if($Update){$_.Result = "AppUpdated";$_
+                }else{$_.Result = "AppInstalled";$_}
+            		
+            #--------------------------------------------------#
+            # condition exclusion
+            #--------------------------------------------------#
+            		
+            }else{
+            	
+                $_.Result = "ConditionExclusion";$_
+            }
+
+        #--------------------------------------------------#
+        # remove app
+        #--------------------------------------------------#
+        	
+        }else{
+            
+            Get-MSI | where{$_.ProductName -eq "VMware vSphere PowerCLI"} | ForEach-Object{
+                 Start-Process -FilePath "msiexec" -ArgumentList "/uninstall $($_.LocalPackage) /qn" -Wait 
+            }
+                
+            $_.Result = "AppUninstalled";$_
+        }
+
+    #--------------------------------------------------#
+    # catch error
+    #--------------------------------------------------#
+
+    }catch{
+
+        $Config.Result = "Error";$Config
+    }
+}
