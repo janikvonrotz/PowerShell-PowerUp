0298e0a528fb8e744581bea4af86b4fcae909ce7
Janik von Rotz <login@janikvonrotz.ch>
2014-05-20 18:22:55 +0200

    new package windirstat

---
+++ lib/App/Example/Install-WinDirStat.ps1
@@ -0,0 +1,110 @@
+param(
+	[string]$Version,
+	[string]$Path,
+	[switch]$Force,
+	$Update,
+	[switch]$Uninstall
+)
+
+#--------------------------------------------------#
+# settings
+#--------------------------------------------------#
+
+$Configs = @{
+	Version = "1.1.2"
+	Url = "http://freefr.dl.sourceforge.net/project/windirstat/windirstat/1.1.2%20installer%20re-release%20%28more%20languages%21%29/windirstat1_1_2_setup.exe"
+    Path = "$(Split-Path -Path $MyInvocation.MyCommand.Definition -Parent)\"
+}
+
+$Configs | where{$_.Version -eq $Version} | ForEach-Object{
+
+    try{
+
+        $_.Result = $null
+        if(-not $_.Path){$_.Path = $Path}
+        $Config = $_
+
+        #--------------------------------------------------#
+        # add app
+        #--------------------------------------------------#
+
+        if(-not $Uninstall){
+
+            #--------------------------------------------------#
+            # check condition
+            #--------------------------------------------------#
+
+            if($_.ConditionExclusion){            
+                $_.ConditionExclusionResult = $(Invoke-Expression $Config.ConditionExclusion -ErrorAction SilentlyContinue)        
+            }    
+            if(($_.ConditionExclusionResult -eq $null) -or $Force){
+                    	
+                #--------------------------------------------------#
+                # download
+                #--------------------------------------------------#
+
+                $_.Downloads = $_.Url | ForEach-Object{
+                    Get-File -Url $_ -Path $Config.Path
+                }       			
+
+                #--------------------------------------------------#
+                # installation
+                #--------------------------------------------------#
+				
+                $_.Downloads | ForEach-Object{
+                    Start-Process -FilePath $(Join-Path $_.Path $_.Filename) -ArgumentList "/S /NORESTART" -Wait
+                }
+                		
+                #--------------------------------------------------#
+                # configuration
+                #--------------------------------------------------#	
+                
+                #--------------------------------------------------#
+                # cleanup
+                #--------------------------------------------------#
+
+                $_.Downloads | ForEach-Object{
+                    Remove-Item (Join-Path $_.Path $_.Filename) -Force
+                }
+                		
+                #--------------------------------------------------#
+                # finisher
+                #--------------------------------------------------#
+                		
+                if($Update){
+                    $_.Result = "AppUpdated";$_
+                }elseif($Downgrade){
+                    $_.Result = "AppDowngraded";$_
+                }else{
+                    $_.Result = "AppInstalled";$_
+                }
+            		
+            #--------------------------------------------------#
+            # condition exclusion
+            #--------------------------------------------------#
+            		
+            }else{
+            	
+                $_.Result = "ConditionExclusion";$_
+            }
+
+        #--------------------------------------------------#
+        # remove app
+        #--------------------------------------------------#
+        	
+        }else{
+
+			$Executable = "C:\Program Files (x86)\WinDirStat\Uninstall.exe"; if(Test-Path $Executable){Start-Process -FilePath $Executable -ArgumentList "/S /NORESTART" -Wait}
+
+            $_.Result = "AppUninstalled";$_
+        }
+
+    #--------------------------------------------------#
+    # catch error
+    #--------------------------------------------------#
+
+    }catch{
+
+        $Config.Result = "Error";$Config
+    }
+}
